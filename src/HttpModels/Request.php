18a9f9be7c7981a31787379a2a3b4d0c7eef70a2
Aldin Kovačević <aldin.kovacevic.97@gmail.com>
2019-03-05 17:18:12 +0100

    Created a Request model for the HttpLogger.

---
+++ src/HttpModels/Request.php
@@ -0,0 +1,166 @@
+<?php
+
+namespace HttpLog\HttpModels;
+use HttpLog\Utils\HeaderUtils;
+
+class Request {
+    private $url;
+    private $base;
+    private $method;
+    private $referrer;
+    private $ip;
+    private $port;
+    private $ajax;
+    private $scheme;
+    private $user_agent;
+    private $type;
+    private $length;
+    private $query;
+    private $data;
+    private $cookies;
+    private $files;
+    private $is_https;
+    private $accept;
+    private $proxy_ip;
+    private $request_headers;
+
+    /**
+     * Construct the Request object.
+     * @return void
+    */
+    public function __construct() {
+        $properties = [
+            "url" => str_replace("@", "%40", $this->get_variable("REQUEST_URI", "/")),
+            "base" => str_replace(array("\\"," "), array("/","%20"), dirname($this->get_variable('SCRIPT_NAME'))),
+            "method" => $this->get_method(),
+            "referrer" => $this->get_variable("HTTP_REFERER"),
+            "ip" => $this->get_variable("REMOTE_ADDR"),
+            "port" => $this->get_variable("REMOTE_PORT"),
+            "scheme" => $this->get_variable("SERVER_PROTOCOL", "HTTP/1.1"),
+            "user_agent" => $this->get_variable("HTTP_USER_AGENT"),
+            "ajax" => $this->get_variable("HTTP_X_REQUESTED_WITH") === "XMLHttpRequest",
+            "type" => $this->get_variable("CONTENT_TYPE"),
+            "length" => $this->get_variable("CONTENT_LENGTH", 0),
+            "query" => $_GET,
+            "data" => $_POST,
+            "cookies" => $_COOKIE,
+            "files" => $_FILES,
+            "is_https" => $this->get_variable("HTTPS", "off") !== "off",
+            "accept" => $this->get_variable("HTTP_ACCEPT"),
+            "request_headers" => HeaderUtils::get_request_headers()
+        ];
+        /* Initialize the Request object */
+        $this->initialize($properties);
+    }
+
+    /**
+     * Initialize Request properties.
+     * @param array $properties Array of request properties.
+     * @return void
+     */
+    private function initialize($properties) {
+        /* Set all defined properties */
+        foreach ($properties as $name => $value) {
+            $this->$name = $value;
+        }
+        /* Perform additional formatting */
+        $this->format_url();
+        $this->format_json_request_body();
+    }
+
+    /**
+     * Get a request variable.
+     * Get a request variable from $_SERVER, or use a $default value.
+     * @param string $variable Variable name.
+     * @param string $default Default value to be used.
+     * @return string Variable value.
+     */
+    private function get_variable($variable, $default = "") {
+        return isset($_SERVER[$variable]) ? $_SERVER[$variable] : $default;
+    }
+
+    /**
+     * Get the request method.
+     * @return string
+     */
+    private function get_method() {
+        $method = $this->get_variable("REQUEST_METHOD", "GET");
+        /* Check for method overrides */
+        if (isset($_SERVER["HTTP_X_HTTP_METHOD_OVERRIDE"])) {
+            $method = $_SERVER["HTTP_X_HTTP_METHOD_OVERRIDE"];
+        }
+        /* Check for alternative method header */
+        if (isset($_REQUEST["_method"])) {
+            $method = $_REQUEST["_method"];
+        }
+        /* Return capitalized method name */
+        return strtoupper($method);
+    }
+
+    /**
+     * Get request body.
+     * Get the body of an HTTP request (used in case of JSON input).
+     * @return string Raw HTTP request body.
+     */
+    private function get_request_body() {
+        static $body;
+        /* If a body already exists, return it*/
+        if (!is_null($body)) {
+            return $body;
+        }
+        /* Get the body from a POST, PUT or PATCH method. */
+        $method = $this->get_method();
+        if (in_array($method, [ "POST", "PUT", "PATCH" ])) {
+            $body = file_get_contents("php://input");
+        }
+        /* Return request body */
+        return $body;
+    }
+
+    /**
+     * Format URL.
+     * Perform additional formatting of URL.
+     * @return string Newly formatted URL.
+     */
+    private function format_url() {
+        /* Get the requested URL without the base directory */
+        if ($this->base != "/" && strlen($this->base) > 0 && strpos($this->url, $this->base) === 0) {
+            $this->url = substr($this->url, strlen($this->base));
+        }
+        /* Set a default URL, if necessary */
+        if (empty($this->url)) {
+            $this->url = '/';
+        }
+    }
+
+    /**
+     * Format request body (if working with JSON).
+     * @return string JSON request body.
+     */
+    private function format_json_request_body() {
+        /* Check for JSON input */
+        if (strpos($this->type, "application/json") === 0) {
+            $body = $this->get_request_body();
+            if ($body !== "") {
+                $data = json_decode($body, true);
+                if ($data !== null) {
+                    $this->data = $data;
+                }
+            }
+        }
+    }
+
+    /**
+     * Return Request properties.
+     *  @var string $criteria
+     * @return array Array of Request properties.
+     */
+    public function get_properties() {
+        $properties = [ ];
+        foreach (get_class_vars(__CLASS__ ) as $property => $value) {
+            $properties[$property] = $this->$property;
+        }
+        /* Return property array */
+        return $properties;
+    }
+}
