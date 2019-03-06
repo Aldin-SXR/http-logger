1f45627f6fb111ad8ce9a0e24b26ef823898eaf7
Aldin Kovačević <aldin.kovacevic.97@gmail.com>
2019-03-06 16:10:17 +0100

    Initial file logging completed (prototype).

--- src/HttpLogger.php
+++ src/HttpLogger.php
@@ -8,15 +8,23 @@
 
 namespace HttpLog;
 use HttpLog\HttpModels\Request;
+use HttpLog\HttpModels\Response;
 
 class HttpLogger {
     private $request;
+    private $response;
 
+    /**
+     * Construct the logger object.
+     * @return void
+     */
     public function __construct() {
         $this->request = new Request();
+        $this->response = new Response;
     }
 
-    public function echo_request() {
-        return $this->request->get_properties();
+    public function log() {
+        $log = json_encode(array_merge($this->request->get_properties(), $this->response->get_properties()));
+        return $log;
     }
 }
