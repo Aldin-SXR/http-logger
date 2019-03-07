e661e4fe593fd0441cfbd23be8e681257b4a0381
Aldin Kovačević <aldin.kovacevic.97@gmail.com>
2019-03-07 16:43:51 +0100

    Refactored the HttpLogger to instead return an instance of a logger type. The log() method will be called individually.

--- src/HttpLogger.php
+++ src/HttpLogger.php
@@ -11,16 +11,16 @@ use HttpLog\Loggers\FileLogger;
 
 class HttpLogger {
     /**
-     * Log the incoming request/response.
-     * Choose the log type and additional log paramters.
+     * Create the incoming request/response logger.
+     * Choose the log type and additional log parameters.
      * @param string $type Log type (file, MySQL or MongoDB).
      * @param string $path Log path.
-     * @return int|boolean Logger response.
+     * @return BaseLogger A subtype of a logger.
      */
-    public static function log($type = "file", $path) {
+    public static function create($type = "file", $path) {
         switch ($type) {
             case "file":
-                return (new FileLogger($path))->log();
+                return new FileLogger($path);
             default:
                 throw new \Exception("Unrecognized log type.");
         }
