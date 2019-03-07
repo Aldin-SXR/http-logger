d03e84baed39abaa866243950091cb81f842f85b
Aldin Kovačević <aldin.kovacevic.97@gmail.com>
2019-03-07 23:42:42 +0100

    Added a method to BaseLogger which sorts filters into request and response filters, and sends those filters to corresponding Request and Response objects.

--- src/Loggers/BaseLogger.php
+++ src/Loggers/BaseLogger.php
@@ -9,19 +9,56 @@
 namespace HttpLog\Loggers;
 use HttpLog\HttpModels\Request;
 use HttpLog\HttpModels\Response;
+use HttpLog\Filters\ParameterFilter;
+use HttpLog\Filters\DefaultFilters;
 
 abstract class BaseLogger {
     protected $request;
     protected $response;
+    protected $log_filter;
 
+    /**
+     * Create a base logger object.
+     * @param string $filter Applied log filter.
+     * @return void
+     */
+    public function __construct($filter) {
+        $this->log_filter = $filter;
+    }
     /**
      * Create log models.
      * Create Request and Response objects, filled with corresponding data.
      * @return void
      */
     protected function create_log_models() {
-        $this->request = new Request();
-        $this->response = new Response();
+        $filters = $this->process_filters();
+        $this->request = new Request($filters["request_filters"]);
+        $this->response = new Response($filters["response_filters"]);
+    }
+
+    /**
+     * Process filters.
+     * Sort all filters into Request and Response filters.
+     * @return array Sorted filter array.
+     */
+    private function process_filters() {
+        $request_filters = [ ];
+        $response_filters = [ ];
+        /* Go through all proposed filters and sort them. */
+        $filters = (new ParameterFilter($this->log_filter))->create_filters();
+        foreach ($filters as $filter) {
+            /** Check for response log filters. */
+            if (in_array($filter, DefaultFilters::RESPONSE_ONLY)) {
+                $response_filters[ ] = $filter;
+            } else {
+                $request_filters[ ] = $filter;
+            }
+        }
+        /* Return sorted filters */
+        return [
+            "request_filters" => $request_filters,
+            "response_filters" => $response_filters
+        ];
     }
 
     /**
